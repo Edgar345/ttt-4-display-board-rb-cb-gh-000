98e4c1b6d730cad5febfceba7129bc2e80ced39f
Edgar V <evenegas9@ucmerced.edu>
2019-08-30 08:24:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board arr
+  puts " #{arr[0]} |"
+  puts " #{arr[1]} |"
+  puts " #{arr[2]} "
+  puts "-----------"
+  puts " #{arr[3]} |"
+  puts " #{arr[4]} |"
+  puts " #{arr[5]} "
+  puts "-----------"
+  puts " #{arr[6]} |"
+  puts " #{arr[7]} |"
+  puts " #{arr[8]}"
+end
